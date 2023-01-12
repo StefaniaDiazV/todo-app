7e4bc747c524d2faaa6eb88e26bcaec21de98629
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-01-12 17:50:15 -0500

    Update/optimizando código form-category

--- src/components/forms/Category/index.tsx
+++ src/components/forms/Category/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -6,43 +6,29 @@ import { categoriesService } from '../../../services';
 import './style.scss'
 
 const CategoryForm = () => {
-
-    const [catName, setCatName ] = useState('')
-
-    const handleSubmit = (e:any) => {
-        e.preventDefault()
-        categoriesService.add({name: catName})
-    }
-
-
     const [name, setName] = useState('')
     const [ifError, setIfError] = useState(false)
 
     const navigate = useNavigate()
     const  { id} = useParams()
 
-    const obtenerCategoria = async () => {
-      if (id)  {
-        const rta = await categoriesService.get(id)
-        setName(rta.name)
+    useEffect(() => {
+        if (id) {
+            categoriesService.get(id).then((rta) => setName(rta.name))
     }
-}
+     }, [id])
 
-    if(id && name === '') obtenerCategoria()
-
-    const enviarFormulario = async (e: any) => {
+    const handelSubmit = async (e: any) => {
         e.preventDefault()
         setIfError(false)
 
         let rta;
-        
         if(id){
             rta = await categoriesService.update({id, name});
         } else {
             rta = await categoriesService.add({ name });
         }
 
-       
         if (rta) {
             setName("");
             navigate('/categories')
@@ -54,7 +40,7 @@ const CategoryForm = () => {
     return (
         <div className='formBox'>
             <div className='addCategoryForm'>
-                <Form onSubmit={enviarFormulario}>
+                <Form onSubmit={handelSubmit}>
                 <h2>Agregar categoria</h2>
                     <Form.Group className="mb-3" controlId="nombre de categoria">
                         <Form.Label>Nombre</Form.Label>
